a7ee991cf5384488b1f9c09576138567db95867f
Abhay maurya <abhaymaurya6013@gmail.com>
2026-01-11 18:22:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+app.use((req, res) => {
+  res.send("hi");
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is running at",process.env.PORT)
+});
