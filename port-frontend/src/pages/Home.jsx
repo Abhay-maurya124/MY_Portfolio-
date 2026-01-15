927a74cc22731f9c5b468acf1796076632ade7e8
Abhay maurya <abhaymaurya6013@gmail.com>
2026-01-15 16:27:52 +0530

    first

--- port-frontend/src/pages/Home.jsx
+++ port-frontend/src/pages/Home.jsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react'
-import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
+
 const Home = () => {
-    //     useGSAP(()=>{
-    //     gsap.to("#box",
-    //         { rotation: "760", duration: 10 }
-    //     )
-    // })
 
-    const [toggle, settoggle] = useState(true)
     return (
-        <div className='  h-screen bg-black text-white'>
-            {toggle ?
-                <div className='w-[500px] h-[500px] flex justify-center items-center rounded-[50%] bg-amber-400'>
-                    Navigate
+        <>
 
-                </div> :
-                <div className='w-[500px] h-[500px] flex justify-center items-center rounded-[50%] bg-amber-400' >
-                    Home
-                    projects
-                    Socials
-                    Contact
-                </div>
-            }
-            <button onClick={() => settoggle(!toggle)}>click</button>
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default Home
+export default Home;
